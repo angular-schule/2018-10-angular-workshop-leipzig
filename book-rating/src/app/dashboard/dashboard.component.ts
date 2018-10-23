eea533ad2e982076dd38d6b7aeb3a13ce68eb5fa
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-10-23 11:44:38 +0200

    Styles einbinden, DashboardComponent, string[]

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: string[];
+
+  constructor() {
+  }
+  
+  ngOnInit() {
+    this.books = ['Angular', 'React', 'Vue.js'];
+  }
+
+}
