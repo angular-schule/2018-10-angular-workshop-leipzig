356f7775887e90cb9ff0e92faf1d3d543338f401
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-10-24 16:47:15 +0200

    BookStoreService mit statischen Büchern

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -10,30 +11,10 @@ export class DashboardComponent implements OnInit {
 
   books: Book[];
 
-  constructor() {
-  }
+  constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.books = [
-      {
-        isbn: '000',
-        title: 'Angular',
-        description: 'Grundlagen, fortgeschrittene Techniken, …',
-        rating: 5
-      },
-      {
-        isbn: '111',
-        title: 'React',
-        description: 'Ein anderes Framework',
-        rating: 3
-      },
-      {
-        isbn: '222',
-        title: 'Vue.js',
-        description: 'Neu, hip und grün',
-        rating: 4
-      },
-    ];
+    this.books = this.bs.getAllStatic();
   }
 
 
