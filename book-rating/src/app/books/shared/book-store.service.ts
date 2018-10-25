1eaf750dd0a9cd3c9cd9262d141532b8fa26fb4e
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-10-25 10:32:47 +0200

    HTTP

--- book-rating/src/app/books/shared/book-store.service.ts
+++ book-rating/src/app/books/shared/book-store.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 import { Book } from './book';
 
 @Injectable({
@@ -6,7 +9,27 @@ import { Book } from './book';
 })
 export class BookStoreService {
 
-  constructor() {}
+  private apiUrl = 'https://api.angular.schule';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiUrl}/books`);
+    // TODO: Mapping auf echte Bücher
+    // TODO: Error Handling
+  }
+
+  getSingle(isbn: string): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/book/${isbn}`);
+  }
+
+  create(book: Book): Observable<any> {
+    return this.http.post(
+      `${this.apiUrl}/book`,
+      book,
+      { responseType: 'text' }
+    );
+  }
 
   getAllStatic(): Book[] {
     return [
